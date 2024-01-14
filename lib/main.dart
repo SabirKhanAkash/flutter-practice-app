15819f8755ea76aa1536ce5bf81b797e52c3911d
Sabir Khan Akash <39434260+SabirKhanAkash@users.noreply.github.com>
2024-01-14 23:43:35 +0600

    added drawer and custom fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 
 main() {
   runApp(const MyApp());
@@ -33,19 +32,9 @@ class _HomeActivityState extends State<HomeActivity> {
       backgroundColor: const Color.fromARGB(255, 0, 73, 120),
       action: SnackBarAction(
           label: 'ঠিক আছে',
+          textColor: Colors.white,
           onPressed: ScaffoldMessenger.of(context).hideCurrentSnackBar),
     ));
-  }
-
-  void showToast(String message) {
-    Fluttertoast.showToast(
-      msg: message,
-      toastLength: Toast.LENGTH_SHORT,
-      gravity: ToastGravity.TOP_RIGHT,
-      backgroundColor: const Color.fromARGB(255, 0, 73, 120),
-      textColor: Colors.white,
-      fontSize: 16.0,
-    );
   }
 
   @override
@@ -55,14 +44,23 @@ class _HomeActivityState extends State<HomeActivity> {
         toolbarHeight: 60,
         backgroundColor: const Color.fromARGB(255, 0, 73, 120),
         centerTitle: true,
-        title: const Text("নৌ টিকিট ব্যবস্থাপনা"),
-        titleTextStyle: const TextStyle(
-            color: Colors.white, fontSize: 18, fontWeight: FontWeight.normal),
+        title: const Text(
+          "নৌ টিকিট ব্যবস্থাপনা",
+          style: TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+              fontWeight: FontWeight.normal,
+              fontFamily: 'balooda'),
+        ),
         actions: [
           IconButton(
             onPressed: () {
               showSnackBar(
-                  "অ্যাপটি তৈরী করেছেন আকাশ 💙 সকল অধিকার সংরক্ষিত", context);
+                  const Text(
+                    "অ্যাপটি তৈরী করেছেন আকাশ 💙 সকল অধিকার সংরক্ষিত",
+                    style: TextStyle(fontFamily: 'balooda'),
+                  ),
+                  context);
             },
             icon: const Icon(Icons.info_outline_rounded),
             color: Colors.white,
@@ -71,6 +69,7 @@ class _HomeActivityState extends State<HomeActivity> {
       ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: currentIndex,
+        selectedItemColor: const Color.fromARGB(255, 0, 73, 120),
         items: const [
           BottomNavigationBarItem(
               icon: Icon(Icons.airplane_ticket_rounded), label: "টিকিট"),
@@ -84,13 +83,71 @@ class _HomeActivityState extends State<HomeActivity> {
             currentIndex = index;
           });
           if (index == 0) {
-            showToast("টিকিট কাটতে পারবেন");
+            showSnackBar("টিকিট কাটতে পারবেন", context);
           } else if (index == 1) {
-            showToast("হিসাব রাখতে পারবেন");
+            showSnackBar("হিসাব রাখতে পারবেন", context);
           } else if (index == 2) {
-            showToast("নিজ প্রোফাইল দেখতে পারবেন");
+            showSnackBar("নিজ প্রোফাইল দেখতে পারবেন", context);
           }
         },
+      ),
+      drawer: Drawer(
+        backgroundColor: const Color.fromARGB(255, 235, 242, 252),
+        child: ListView(
+          children: [
+            const DrawerHeader(
+                padding: EdgeInsets.all(0),
+                child: UserAccountsDrawerHeader(
+                    decoration: BoxDecoration(color: Color.fromARGB(255, 0, 73, 120)),
+                    accountName: Text("নৌ টিকিট ব্যবস্থাপনায়\nআপনাকে স্বাগতম",
+                        style: TextStyle(
+                            fontFamily: 'balooda',
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white)),
+                    accountEmail: Text("nou.support@gmail.com"))),
+            ListTile(
+                title: const Text("হোমপেজ",
+                    style: TextStyle(fontFamily: 'balooda')),
+                leading: const Icon(Icons.home_rounded,
+                    color: Color.fromARGB(255, 0, 73, 120)),
+                onTap: () {
+                  showSnackBar("হোমপেজ চেপেছেন", context);
+                }),
+            ListTile(
+                title: const Text("গোপনীয়তা শর্তাবলী",
+                    style: TextStyle(fontFamily: 'balooda')),
+                leading: const Icon(Icons.info_outline_rounded,
+                    color: Color.fromARGB(255, 0, 73, 120)),
+                onTap: () {
+                  showSnackBar("গোপনীয়তা শর্তাবলী চেপেছেন", context);
+                }),
+            ListTile(
+                title: const Text("নীতিমালা",
+                    style: TextStyle(fontFamily: 'balooda')),
+                leading: const Icon(Icons.edit_document,
+                    color: Color.fromARGB(255, 0, 73, 120)),
+                onTap: () {
+                  showSnackBar("নীতিমালা চেপেছেন", context);
+                }),
+            ListTile(
+                title: const Text("ভার্শন চেক করুন",
+                    style: TextStyle(fontFamily: 'balooda')),
+                leading: const Icon(Icons.update_rounded,
+                    color: Color.fromARGB(255, 0, 73, 120)),
+                onTap: () {
+                  showSnackBar("ভার্শন চেক করুন চেপেছেন", context);
+                }),
+            ListTile(
+                title: const Text("লগ আউট করুন",
+                    style: TextStyle(fontFamily: 'balooda')),
+                leading: const Icon(Icons.logout_rounded,
+                    color: Color.fromARGB(255, 0, 73, 120)),
+                onTap: () {
+                  showSnackBar("লগ আউট চেপেছেন", context);
+                }),
+          ],
+        ),
       ),
       backgroundColor: const Color.fromARGB(255, 235, 242, 252),
     );
